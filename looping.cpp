8fc7254a217f45fed689e54a27de3ca684373ce1
Regan <52940978+reganjs@users.noreply.github.com>
2021-03-11 20:47:08 -0600

    Update and rename TMA1Question3.cpp to looping.cpp

--- looping.cpp
+++ looping.cpp
@@ -1,6 +1,4 @@
-//Regan Setter
-//Student ID 3505894
-//Computer Science 206
+//Regan
 //January 12th, 2021
 
 //Write a program that uses two nested for loops and the modulus operator (%) to detect and print the prime numbers from 1 to 10,000. (Prime numbers are natural numbers that are not evenly divisible by any other number except for themselves and one). Display all the primes found.
